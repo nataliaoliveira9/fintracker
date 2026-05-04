526ba2d5c5e5d6501fc9482e3ace58b0235c7bd6
Natália <natyoliveira@proton.me>
2026-05-03 23:17:57 -0300

    fixed django settings

--- backend/fintracker/settings.py
+++ backend/fintracker/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
